6314b9fcac9224203ae792ea6087aaac8a04eb7f
Olga Marchenko <olga_marchenko1@epam.com>
2023-12-07 04:32:47 +0000

    Remote unnecesary lines

--- script1.sh
+++ script1.sh
@@ -2,15 +2,4 @@
 fileDir='/tmp/'
 fileName='first_bash_output'
 
-
-#currDate=$(date)  
-#currDir=$(pwd)
-
-#echo Current date : $currDate
-#echo Current dir : $currDir
-
-#echo -e "Text: ${currDate}\n${currDir}" 
- 
-#echo File path: ${fileDir}${fileName}
-
 echo -e "$(date)\n$(pwd)" > ${fileDir}${fileName} 
